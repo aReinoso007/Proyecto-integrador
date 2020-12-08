202c0c14af811c367980c210790955c8a55edcdd
Alex Reinoso <ralexjessiel@gmail.com>
2020-12-08 13:52:31 -0500

    Metodo detectar movimiento

--- Principal.cpp
+++ Principal.cpp
@@ -4,7 +4,7 @@ Mat frame, actual, anterior, resta, frame2, gaus, mediana, erosion, apertura, ci
 int mascaraMediana = 0;
 int mascaraGausiana = 0;
 int kernel=3;
-
+int pixel= 0;
 void eventoTrack(int v, void *p){
     cout << "Valor: " << v << endl;
 }
@@ -17,15 +17,16 @@ void Dilatar(){
 }
 
 Mat aplicarMediana(Mat src, Mat dest, int mascara){
+    Mat resultado;
     if(mascara%2 == 0){
         medianBlur(src, dest, mascara+1);
         cout << "Valor debe ser impar, Mascara Mediana: " <<mascara << "\n";
-        return dest;
+        resultado = dest;
     }else{
         medianBlur(src, dest, mascara);
-        return dest;
+        resultado = dest;
     };
-    return dest;
+    return resultado = dest;
 }
 
 Mat aplicarGaussiana(Mat src, Mat dest, int mascara){
@@ -41,6 +42,36 @@ Mat aplicarGaussiana(Mat src, Mat dest, int mascara){
 
     return dest;
 }
+Mat detectarMovimiento(Mat frame){
+    Mat resultado;
+    resultado = frame.clone();
+    cvtColor(frame, frame, COLOR_BGR2GRAY);
+    actual = frame.clone();
+    if(anterior.rows==0 || anterior.cols==0){
+        anterior = frame.clone();
+    }
+    absdiff(actual, anterior, resta);
+    anterior = actual.clone();
+    threshold(resta, resta, 33, 255, THRESH_BINARY);
+    
+    for(int i=0;i<resultado.rows;i++){
+        for(int j=0;j<resultado.cols;j++){
+            pixel = (int) resta.at<uchar>(i,j);
+            if (pixel != 0){
+            break;
+            }
+            resultado.at<Vec3b>(i,j) = resta.at<Vec3b>(); 
+        }
+        for(int j=resta.cols-1;j>=0;j--){
+            pixel = (int) resta.at<uchar>(i,j);
+            if (pixel != 0)
+                break;
+            resultado.at<Vec3b>(i,j) = resta.at<Vec3b>(); 
+        }
+    }
+
+    return resultado;
+}
 
 int main(int argc, char *argv[]){
 
@@ -54,6 +85,7 @@ int main(int argc, char *argv[]){
         createTrackbar("Filtro Gausiano", "Video", &mascaraGausiana, 11, eventoTrack, NULL);
         createTrackbar("Kernel", "Video", &kernel, 39, eventoTrack, NULL);
         Mat tamanio = getStructuringElement(MORPH_CROSS, Size(kernel, kernel));
+
         while(true){
             video >> frame;
             video >> erosion;
@@ -69,6 +101,7 @@ int main(int argc, char *argv[]){
 
             gaus = aplicarGaussiana(frame, gaus, mascaraGausiana);
             mediana = aplicarMediana(frame, mediana, mascaraMediana);
+            frame2 = detectarMovimiento(frame);
 
             if(kernel%2 ==0){
                 tamanio = getStructuringElement(MORPH_CROSS, Size(kernel+1, kernel+1));
@@ -82,35 +115,6 @@ int main(int argc, char *argv[]){
                 morphologyEx(frame, blackhat, MORPH_BLACKHAT, tamanio);
             };
             
-            /*Esta parte para el detector de movimiento */
-            cvtColor(frame, frame, COLOR_BGR2GRAY);
-            actual = frame.clone();
-            if(anterior.rows==0 || anterior.cols==0){
-                anterior = frame.clone();
-            }
-            absdiff(actual, anterior, resta);
-            anterior = actual.clone();
-            threshold(resta, resta, 33, 255, THRESH_BINARY);
-
-            int pixel= 0;
-            for(int i=0;i<frame2.rows;i++){
-                for(int j=0;j<frame2.cols;j++){
-            
-                    pixel = (int) resta.at<uchar>(i,j);
-                    if (pixel != 0){
-                    break;
-                    }
-                    frame2.at<Vec3b>(i,j) = resta.at<Vec3b>(); 
-                }
-                for(int j=resta.cols-1;j>=0;j--){
-                    pixel = (int) resta.at<uchar>(i,j);
-                    if (pixel != 0)
-                        break;
-                    frame2.at<Vec3b>(i,j) = resta.at<Vec3b>(); 
-                }
-            }
-
-            //cvtColor(frame, frame, COLOR_GRAY2BGR);
             imshow("Video", frame);
             imshow("Gausiano", gaus);
             imshow("Mediana", mediana);
